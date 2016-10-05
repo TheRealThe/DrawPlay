30e232f0e1ee223d90a393558a13817948832490
The Ngo <The@linuxnewz.com>
2016-10-05 15:21:55 -0600

    Moved mouse to a different position

--- DrawPlay/src/MyDrawing.java
+++ DrawPlay/src/MyDrawing.java
@@ -17,7 +17,7 @@ public class MyDrawing extends JPanel {
 	// to display (e.g., when the program starts)
 	public void paintComponent(Graphics g) {
 		cat.draw(g, 50, 50);
-		mouse.draw(g,200, 100);
+		mouse.draw(g,100, 200);
 	}
 	
 	/**
